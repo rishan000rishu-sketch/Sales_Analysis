6b8d455dc01eaaf332606cce77587b776360a121
Muhammed Rishan T <rishan000rishu@gmail.com>
2026-06-03 11:26:20 +0530

    Added bar chart and pie chart

--- Sales_Analysis.py
+++ Sales_Analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_excel('sales_data.xlsx', sheet_name='data')
 
@@ -26,3 +27,22 @@ print(customer_sales.sort_values(ascending=False))
 
 df.to_excel('sales_data.xlsx', sheet_name='data',index=False)
 print('\nReport saved succesfuly')
+
+#Created product sales chart
+product_sales.plot(kind='bar')
+
+plt.title('Product Sales')
+plt.xlabel('Product')
+plt.ylabel('Revenue')
+
+plt.show()
+
+#Created city sales pie chart
+city_sales.plot(
+    kind='pie',
+    autopct = '%1.1f%%'
+)
+
+plt.title('Sales by city')
+plt.ylabel('')
+plt.show()
